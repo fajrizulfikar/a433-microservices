b11f4ff86bbf9405055daea04a31ba0e8ee3dfe3
Zulfikar Fajri <zulfikar.fajri@admo.astra.co.id>
2024-03-26 15:15:21 +0700

    feat: add sh file to automate build and push image processes

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,22 @@
+# check if all arguments passed to the script
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <your_username> <your_token> <your_repository>"
+    exit 1
+fi
+
+# bind argument to variable
+username=$1
+token=$2
+repository=$3
+
+# build image
+docker build -t $username/karsajobs:latest .
+
+# prepare the tag for the github packages
+docker tag $username/karsajobs:latest ghcr.io/$username/$repository/karsajobs:latest
+
+# login to github packages
+docker login ghcr.io -u $username -p $token
+
+# push image to github packages
+docker push ghcr.io/$username/$repository/karsajobs:latest
